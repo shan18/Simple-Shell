721a96dbc4400b7bdb422ce9696c242b8b629949
Shantanu Acharya <thegeek.004@gmail.com>
2021-10-11 14:49:33 -0400

    run external commands on two paths

--- cmd.c
+++ cmd.c
@@ -2,8 +2,36 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
 #include "cmd.h"
+
+#define EXIT_SUCCESS 0
+#define EXIT_FALIURE -1
+
+void execute_child(char **args) {
+    char *envp[] = {"PATH=/bin/", 0};
+    char abs_cmd[100] = "/bin/";
+    strcat(abs_cmd, args[0]);
+    if(execve(abs_cmd, args, envp) == -1) {
+        envp[0] = "PATH=/usr/bin/";
+        strcpy(abs_cmd, "/usr/bin/");
+        strcat(abs_cmd, args[0]);
+        if(execve(abs_cmd, args, envp) == -1) {
+            fprintf(stderr, "Error: Invalid program\n");
+            exit(EXIT_FALIURE);
+        }
+    }
+}
+
+void run_external(char **args) {
+    int status;
+    if(fork()) {
+        waitpid(-1, &status, 0);
+    } else {
+        execute_child(args);
+    }
+}
 
 void run_cmd(char **args) {
     if (*args[0] == '/') {
@@ -11,16 +39,6 @@ void run_cmd(char **args) {
     } else if(strstr(args[0], "./") == args[0]) {
         return;
     } else if(strcmp(args[0], "ls") == 0) {
-        int status;
-        if (fork()) {
-            waitpid(-1, &status, 0);
-        } else {
-            char *envp[] = {"PATH=/bin/", 0};
-            char ee[] = "/bin/";
-            strcat(ee, args[0]);
-            if(execve(ee, args, envp) == -1) {
-                fprintf(stderr, "Error: Invalid program\n");
-            }
-        }
+        run_external(args);
     }
 }
--- init.h
+++ init.h
@@ -1,10 +1,6 @@
 #ifndef _INIT_H_
 #define _INIT_H_
 
-void show_prompt();
-void read_line(char *buffer, int max_buffer_size);
-void tokenize(char *line, char **args);
-void fix_cmd_path(char *command);
 void get_shell_input(char **args, int max_buffer_size);
 void free_args(char **args, ...);
 
