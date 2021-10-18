c1f63574053ad66d304a89a05cb775652b9281b0
Shantanu Acharya <thegeek.004@gmail.com>
2021-10-17 22:51:43 -0400

    add grammar parsing rules

--- validator.c
+++ validator.c
@@ -3,8 +3,7 @@
 
 #include "validator.h"
 
-const char *TERMINALS[] = {"<", ">", ">>", NULL}; 
-const char *BUILTINS[] = {"cd", "jobs", "fg", "exit", NULL};
+const char *TERMINALS[] = {"<", ">", ">>", NULL};
 
 void show_invalid() {
     fprintf(stderr, "Error: invalid command\n");
@@ -16,7 +15,7 @@ void show_invalid_msg(char *msg) {
 
 int validate_num_args(char *args[], int num) {
     int count;
-    for(count = 0; args[count] != NULL; count++);
+    for (count = 0; args[count] != NULL; count++);
     if (count != num) {
         show_invalid();
         return 0;
@@ -25,14 +24,14 @@ int validate_num_args(char *args[], int num) {
 }
 
 int detect_edge_str(char *args[], int args_len, char const *str) {
-    if(strcmp(args[0], str) == 0 || strcmp(args[args_len - 1], str) == 0)
+    if (strcmp(args[0], str) == 0 || strcmp(args[args_len - 1], str) == 0)
         return 1;
     return 0;
 }
 
 int detect_multi_count(char *args[], char const *str) {
     int flag = 0;
-    for(int i = 0; args[i] != NULL; i++) {
+    for (int i = 0; args[i] != NULL; i++) {
         if (strcmp(args[i], str) == 0) {
             if (flag)
                 return 1;
@@ -43,30 +42,53 @@ int detect_multi_count(char *args[], char const *str) {
 }
 
 int detect_single_count(char *args[], char const *str) {
-    for(int i = 0; args[i] != NULL; i++) {
+    for (int i = 0; args[i] != NULL; i++) {
         if (strcmp(args[i], str) == 0)
             return 1;
     }
     return 0;
 }
 
+int validate_terminal_grammar(char *args[], int idx, int args_len) {
+    return idx + 2 == args_len || (strcmp(args[idx + 2], "<") == 0 && idx + 4 == args_len);
+}
+
+int validate_stdin_grammar(char *args[], int idx, int args_len) {
+    return idx + 2 == args_len || strcmp(args[idx + 2], "|") == 0 || strcmp(args[idx + 2], ">") == 0 || strcmp(args[idx + 2], ">>") == 0;
+}
+
 int validate_args(char *args[]) {
     // Get args count
     int args_len;
-    for(args_len = 0; args[args_len] != NULL; args_len++);
+    for (args_len = 0; args[args_len] != NULL; args_len++);
 
     // Validate terminate strings
     for (int i = 0; TERMINALS[i] != NULL; i++) {
-        if(detect_edge_str(args, args_len, TERMINALS[i]) || detect_multi_count(args, TERMINALS[i])) {
+        if (detect_edge_str(args, args_len, TERMINALS[i]) || detect_multi_count(args, TERMINALS[i])) {
             show_invalid();
             return 0;
         }
     }
 
     // Validate recursive strings and detect invalid strings
-    if(detect_edge_str(args, args_len, "|") || detect_single_count(args, "<<")) {
+    if (detect_edge_str(args, args_len, "|") || detect_single_count(args, "<<")) {
         show_invalid();
         return 0;
+    }
+
+    // Validate grammar
+    for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], ">") == 0 || strcmp(args[i], ">>") == 0) {
+            if (!validate_terminal_grammar(args, i, args_len)) {
+                show_invalid();
+                return 0;
+            }
+        } else if (strcmp(args[i], "<") == 0) {
+            if (!validate_stdin_grammar(args, i, args_len)) {
+                show_invalid();
+                return 0;
+            }
+        }
     }
 
     return 1;
@@ -76,15 +98,15 @@ int validate_pipe_line(char *line) {
     int start, end;
     
     // Check if line begins with |
-    for(start = 0; line[start] == ' '; start++);
-    if(line[start] == '|') {
+    for (start = 0; line[start] == ' '; start++);
+    if (line[start] == '|') {
         show_invalid();
         return 0;
     }
 
     // Check if line ends with |
-    for(end = strlen(line) - 1; line[end] == ' '; end--);
-    if(line[end] == '|') {
+    for (end = strlen(line) - 1; line[end] == ' '; end--);
+    if (line[end] == '|') {
         show_invalid();
         return 0;
     }
@@ -107,8 +129,8 @@ int validate_pipe_args(char *args[], int idx, int max_len) {
 }
 
 int validate_pipe_builtins(char *cmd, char *builtins[], int num_builtins) {
-    for(int i = 0; i < num_builtins; i++) {
-        if(strcmp(cmd, builtins[i]) == 0) {
+    for (int i = 0; i < num_builtins; i++) {
+        if (strcmp(cmd, builtins[i]) == 0) {
             show_invalid();
             return 0;
         }
